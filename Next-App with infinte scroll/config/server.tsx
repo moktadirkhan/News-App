f16547b54f9d67773c71ec04b681513dac1cf19b
Moktadir Khan <moktadir@algomatrix.co>
2021-07-05 18:03:19 +0600

    initial commit

---
+++ Next-App with infinte scroll/config/server.tsx
@@ -0,0 +1,5 @@
+  export const server= {
+    APPLICATION_ID: process.env.APPLICATION_ID || 'L5lQL_0LSnOAc0qJw6kwcc5P63Yx2oFmY2h5r6MDvww',
+    SECRET: process.env.SECRET || 'CLvAusj83_01UeATbB2kbhY3Jzuxxh4K1YkcRIcIDXY',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+  };
---
+++ Next-App with infinte scroll/type.ts
@@ -0,0 +1,6 @@
+export interface IService{
+    name:string,
+    position:string,
+    image:string,
+    description:string
+}
